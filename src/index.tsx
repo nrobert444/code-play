37f0839f14be18171f96a69d5895e479db028b9c
Nicholas Roberto <njr444@gmail.com>
2021-02-16 11:18:17 -0800

    added unpkg, axios and esbuild. Built esbuild/unpkg basic plugin config.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,51 @@
+import * as esbuild from 'esbuild-wasm'
+import { useState, useEffect, useRef } from 'react'
 import ReactDom from 'react-dom'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
-const App = () => {
-  return <h1>hello</h1>
+export const App = () => {
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+  const ref = useRef<any>()
+
+  useEffect(() => {
+    startService()
+  }, [])
+
+  // initialize esbuild
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: './esbuild.wasm'
+    })
+  }
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
+    })
+
+    // console.log(result)
+
+    setCode(result.outputFiles[0].text)
+  }
+
+  return (
+    <div>
+      <textarea value={input} onChange={e => setInput(e.target.value)} />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
 }
 
 ReactDom.render(<App />, document.querySelector('#root'))
---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,44 @@
+import * as esbuild from 'esbuild-wasm'
+import axios from 'axios'
+
+export const unpkgPathPlugin = () => {
+  return {
+    //mostly for debugging
+    name: 'unpkg-path-plugin',
+    //
+    setup(build: esbuild.PluginBuild) {
+      //hijacks esbuild trying to find the packages filepath and allows us to tell it what to look for.
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log('onResolve', args)
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' }
+        } else if (args.path === 'tiny-test-pkg') {
+          return {
+            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+            namespace: 'a'
+          }
+        }
+      })
+      //we provide the package to be loaded then return control to esbuild
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args)
+
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: `
+            const message = require('tiny-test-pkg)
+              console.log(message);
+            `
+          }
+        }
+
+        const { data } = await axios.get(args.path)
+        return {
+          loader: 'jsx',
+          contents: data
+        }
+      })
+    }
+  }
+}
